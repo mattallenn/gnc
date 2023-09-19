32067db1f55cf2ef6bd629418f9114f53afe10de
mattallenn <104325813+mattallenn@users.noreply.github.com>
2023-09-18 20:59:50 -0400

    Began adding PID controller.

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+
+    let mut input = 0.0; // Sensor input
+    let mut setpoint = 0.0;
+    let mut prev_error = 0.0;
+    let mut integral = 0.0;
+    let mut error = 0.0; 
+
+    loop {
+        error = setpoint - input; 
+        integral = integral
+    }
+}
