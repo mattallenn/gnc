89b0bac4bfa1b16ff72ff30b574e87bd602569e8
mattallenn <104325813+mattallenn@users.noreply.github.com>
2024-09-17 09:49:54 -0400

    updated main control loop

--- 9-21-test.py
+++ 9-21-test.py
@@ -130,7 +130,10 @@ def test4():
     file.close()
 
 
+#--------------------------------
 # Main control Loop
+#--------------------------------
+
 if __name__ == "main":
     print(r"""\ 
             ______  _______  ______        __    _____          __           __
@@ -141,3 +144,25 @@ if __name__ == "main":
          | |/ / -_) __(_-</ / _ \/ _ \   / // // /                           
          |___/\__/_/ /___/_/\___/_//_/  /_(_)___/                            
                                                                      """)
+
+    test_selected = input("Please select a test to run:\n 1. Equality test\n 2. Left thrust test\n 3. Right thrust test\n 4. Open nozzle until empty test\n")
+
+    if test_selected == "1":
+        print("Running Equality test")
+        test1()
+        print("Test complete")
+    elif test_selected == "2":
+        print("Running Left thrust test")
+        test2()
+        print("Test complete")
+    elif test_selected == "3":
+        print("Running Right thrust test")
+        test3()
+        print("Test complete")
+    elif test_selected == "4":
+        print("Running Open nozzle until empty test")
+        test4()
+        print("Test complete")
+
+    
+
