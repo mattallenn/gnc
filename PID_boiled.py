537180ad9c641c258c6fe8f35083d5e6e09f595c
mattallenn <104325813+mattallenn@users.noreply.github.com>
2024-10-30 19:51:42 -0400

    added tracking to PID_Boiled.py

--- PID_boiled.py
+++ PID_boiled.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 from BMI160_i2c import Driver
 
 print('Trying to initialize the sensor...')
@@ -15,21 +15,40 @@ GPIO.setup(relay2, GPIO.OUT)
 
 # Example usage
 if __name__ == "__main__":
-    while True:
-        data = sensor.getMotion6()
-        current_value = data[1] / 16000
-        
-        if current_value > 0.05:
-            # Fire left
-            GPIO.output(relay2, GPIO.LOW)
-            sleep(0.1)
-            GPIO.output(relay2, GPIO.HIGH)
 
-        elif current_value < -0.05:
-            # Fire left
-            GPIO.output(relay1, GPIO.LOW)
-            sleep(0.1)
-            GPIO.output(relay1, GPIO.HIGH)
+    try:
+        current_time4 = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        file_name = f"test_data/PID_boiled_{current_time4}.csv"
+        file = open(file_name, "w")
+
+        # Write a header to the file
+        file.write("Nozzle 1 state,Nozzle 2 state,Time,Vel Y\n")
+        start_time = time.time()
+
+        while True:
+            data = sensor.getMotion6()
+            current_value = data[1] / 16000
+
+            current_time = time.time()
+            data = sensor.getMotion6()
+            file.write(f"{GPIO.input(relay1)},{GPIO.input(relay2)}, {round(time.time()-start_time,2)},{data[1]}\n")
             
-    GPIO.output(relay1, GPIO.HIGH)
-    GPIO.output(relay2, GPIO.HIGH)
+            if current_value > 0.05:
+                # Fire left
+                GPIO.output(relay2, GPIO.LOW)
+                sleep(0.1)
+                GPIO.output(relay2, GPIO.HIGH)
+
+            elif current_value < -0.05:
+                # Fire left
+                GPIO.output(relay1, GPIO.LOW)
+                sleep(0.1)
+                GPIO.output(relay1, GPIO.HIGH)
+                
+        GPIO.output(relay1, GPIO.HIGH)
+        GPIO.output(relay2, GPIO.HIGH)
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        file.close()
+        print('Test aborted, cleaning up GPIO and closing file')
