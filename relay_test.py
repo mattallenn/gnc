7132ca6ca2aacab407af290002c1164a7b36dca1
Matt Allen <matthewallen442@gmail.com>
2024-10-30 20:00:56 -0400

    updated files

--- relay_test.py
+++ relay_test.py
@@ -12,11 +12,11 @@ GPIO.setup(relay2, GPIO.OUT)
 if __name__ == "__main__":
 
     while True:
-#      GPIO.output(relay1, GPIO.HIGH)
+        GPIO.output(relay1, GPIO.HIGH)
         GPIO.output(relay2, GPIO.HIGH)
 
         time.sleep(0.5)
-#        GPIO.output(relay1, GPIO.LOW)
+        GPIO.output(relay1, GPIO.LOW)
         GPIO.output(relay2, GPIO.LOW)
         time.sleep(0.5)
 
