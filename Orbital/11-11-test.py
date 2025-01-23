aaeb80184df7d380f518a8d3d6cf76b056619c5c
mattallenn <104325813+mattallenn@users.noreply.github.com>
2025-01-23 12:04:13 -0500

    added some notes for polaris

--- Orbital/11-11-test.py
+++ Orbital/11-11-test.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 
 # Load the CSV file
 # Replace 'your_file.csv' with the actual path to your CSV file
-data = pd.read_csv('.//11-11-test/PID_boiled_4.csv')
+data = pd.read_csv('.//11-11-test/PID_boiled_3.csv')
 
 # Extract the relevant columns (Time and Vel Y)
 time = data['Time']
-velocity = data['Vel Y']
+velocity = data['Vel Y'] / 16000
 
 # Plot Time vs. Velocity
 plt.figure(figsize=(10, 6))
 plt.plot(time, velocity, marker='o', color='b', linestyle='-')
 plt.title('Time vs Velocity')
 plt.xlabel('Time (s)')
-plt.ylabel('Velocity (Vel Y)')
+plt.ylabel('Angular Velocity (Rotations per Second)')
 plt.grid(True)
 
 # Show the plot
---
+++ notes.md
@@ -0,0 +1,9 @@
+# Notes and References for Polaris Design
+
+### Electrical Systems
+
+- **IMU:** [BNO055](https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/overview)
+
+- Helpful Links:
+    - [Calibration Steps](https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/device-calibration)
+    - [WebGL Example](https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/webgl-example)
